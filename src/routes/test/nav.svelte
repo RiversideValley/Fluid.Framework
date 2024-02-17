6d9696303157f02ddced122e4bf9c43aef55210f
Lamparter <lamparter@theriversi.de>
2024-02-17 16:33:29 +0000

    🧱 Remove website infrastructure

--- src/routes/test/nav.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { NavigationView, TextBlock } from "@riversidevalley/fluid";
-</script>
-
-<NavigationView>
-	<TextBlock variant="title">Page Title</TextBlock>
-</NavigationView>
